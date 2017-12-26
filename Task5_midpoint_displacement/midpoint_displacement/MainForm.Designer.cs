bf14e5dd11d47e96c84cccea3888efdf47d4733d
Вадим <Vadik426_426@mail.ru>
2017-12-26 11:44:14 +0300

    Add files via upload

---
+++ Task5_midpoint_displacement/midpoint_displacement/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace midpoint_displacement
+{
+	partial class MainForm
+	{
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pictureBox1.Location = new System.Drawing.Point(87, 13);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(570, 305);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pct_click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(40, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(23, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "R";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(13, 33);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(68, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 60);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(68, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "make";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Click_make);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(13, 90);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(68, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "clear";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Click_clear);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(669, 330);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "MainForm";
+			this.Text = "midpoint_displacement";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+	}
+}
---
+++ Task5_midpoint_displacement/midpoint_displacement/MainForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace midpoint_displacement
+{
+	public partial class MainForm : Form
+	{
+		Bitmap bmp;
+		private System.Collections.Generic.List<System.Drawing.Point> pts = new List<Point>();
+		int clicks = 0;
+		public MainForm()
+		{
+			InitializeComponent();
+			bmp=new Bitmap(pictureBox1.Width,pictureBox1.Height);
+			this.pictureBox1.Image=bmp;
+		}
+		
+		void clear(){
+			bmp.Dispose();
+			bmp=new Bitmap(pictureBox1.Width,pictureBox1.Height);
+			this.pictureBox1.Image=bmp;
+		}
+		
+		void Click_clear(object sender, System.EventArgs e){
+			//this.pictureBox1.Image.Dispose();
+			clear();
+			clicks=0;
+			pts = new List<Point>();
+		}
+		
+		void draw_points(Graphics g){
+			for(int i = 0; i < pts.Count; i++)
+				g.DrawLine(new Pen(Color.Black, 3),pts[i].X-1,pts[i].Y-1,pts[i].X+1,pts[i].Y-1);
+		}
+		
+		void pct_click(object sender, System.Windows.Forms.MouseEventArgs e){
+			if (clicks > 1)
+			 return;
+			clicks++;
+			Graphics g = Graphics.FromImage(bmp);
+			g.DrawLine(new Pen(Color.Black, 3),e.X-1,e.Y-1,e.X+1,e.Y-1);
+			pts.Add(new Point(e.X,e.Y));
+			g.Dispose();
+			pictureBox1.Invalidate();
+		}
+		
+		int length(Point p1, Point p2){
+			return (int)Math.Sqrt((p2.X-p1.X)*(p2.X-p1.X)+(p2.Y-p1.Y)*(p2.Y-p1.Y))/10;
+		}
+		
+		void midpoint_displacement(Point pt1, Point pt2, int r, int ind_r){
+			if (pt2.X-pt1.X < 2) return;
+			
+			int l = length(pt1,pt2);
+			Random rnd = new Random();
+			int h = (pt1.Y + pt2.Y) / 2 + rnd.Next(- r * l, r * l + 1);
+			int w = (pt1.X+pt2.X)/2;
+			Point mid = new Point(w,h);
+			pts.Insert(ind_r, mid);
+			clear();
+			
+			{
+				Point[] ppts = pts.ToArray();
+				Graphics g = Graphics.FromImage(bmp);
+				g.DrawLines(new Pen(Color.Black, 1), ppts);
+				draw_points(g);
+				g.Dispose();
+				System.Threading.Thread.Sleep(100);
+			}
+			pictureBox1.Update();
+			
+			midpoint_displacement(mid,pt2,r,ind_r+1);
+			midpoint_displacement(pt1,mid,r,ind_r);
+		}
+		
+		void Click_make(object sender, System.EventArgs e){
+			midpoint_displacement(pts[0],pts[1],Int32.Parse(textBox1.Text),1);
+		}
+	}
+}
